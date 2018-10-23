30a9c46dd1056e4897938ba27827229fdaa03137
Jonathan Lee <jonathan.hyunho.lee@gmail.com>
2018-10-23 02:19:53 -0700

    added node server and added upload route

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static('dist'));
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(fileUpload());
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
+app.post('/upload', (req, res) => {
+  const imageFile = req.files.imgFile;
+
+  imageFile.mv(`${__dirname}/public/uploads/${req.body.imgName}`, (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    res.json({ file: `public/uploads${req.body.imgName}` });
+    return res.status(200).send('success');
+  });
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+
+module.exports = app;
